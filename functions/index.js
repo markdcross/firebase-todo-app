d328e906c94b8700336d474588fb2b3b26e1bfe5
MDC <markdcross@gmail.com>
2021-02-24 11:46:50 -0500

    Connected to firebase and initial route created

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const { getAllTodos } = require('./APIs/todos');
+
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
