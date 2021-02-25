19c3ab6f903db5ea1d03651951410fb48da8dc94
MDC <markdcross@gmail.com>
2021-02-25 10:17:33 -0500

    Login route created

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+//* Todos
 const {
   getAllTodos,
   postOneTodo,
@@ -15,5 +16,10 @@ app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 
 app.put('/todo/:todoId', editTodo);
+
+//* Users
+const { loginUser } = require('./APIs/users');
+
+app.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyDCwXB-5Bd79ZzUdE5wRuXqQjrdh_QUhRs',
+  authDomain: 'todoapp-5914d.firebaseapp.com',
+  projectId: 'todoapp-5914d',
+  storageBucket: 'todoapp-5914d.appspot.com',
+  messagingSenderId: '753336809514',
+  appId: '1:753336809514:web:9144c4a55b88f261306b53',
+  measurementId: 'G-7HDJ663XBV'
+};
